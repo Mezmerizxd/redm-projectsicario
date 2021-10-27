89327318526695a2146f0ea16cb58d59ce428eb1
SpetsnazSicario <52637194+SpetsnazSicario@users.noreply.github.com>
2021-10-27 13:21:12 +0100

    restart

--- resources/[projectsicario]/ps-adminmenu/src/client/client.ts
+++
@@ -1,9 +0,0 @@
-import AdminMenuManager from "./adminmenuManager";
-import NuiManager from "../../../ts-shared/src/client/managers/nui-manager";
-
-const adminmenu = AdminMenuManager.getInstance();
-const nuiManager = NuiManager.getInstance();
-
-RegisterCommand("menu", () => {
-  nuiManager.DisplayNui("nui-adminmenu", true);
-}, true);
--- resources/[projectsicario]/ps-core/src/index.tsx
+++
@@ -1,9 +0,0 @@
-// Modules
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(
-  <React.StrictMode>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
